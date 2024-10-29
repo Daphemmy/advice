4a029eb0f416b68e7784ea21d61d4d55483d73f2
Daphemmy <benedictadeniran@gmail.com>
2024-10-29 16:51:00 +0000

    first commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let button = document.querySelector(".glow");
 
 let adviceData = async () => {
   let response = await fetch("https://api.adviceslip.com/advice");
-  console.log(response);
+  //   console.log(response);
   let data = await response.json();
   loading.classList.add("d-none");
   adviceID.textContent = `ADVICE #${data.slip.id}`;
